3465c032a4420276670ad84bd3152bb1875dec1d
KoenBisschop <koen.bisschop@hotmail.be>
2020-05-04 13:57:05 +0200

    versieLeandro

---
+++ Raadspelletje/Raadspel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raadspelletje
+{
+    public class Raadspel
+    {
+        private static Random rnd = new Random();
+        public int AantalBeurten { get; set; }
+        public Boolean gewonnen = false;
+        public Boolean einde = false;
+        public int teRaden { get; private set; }
+        public void Worp()
+        {
+            teRaden = rnd.Next(1, 11);
+        }
+        public void Gok(int gok)
+        {
+            if (gok == teRaden)
+                gewonnen = true;
+            else gewonnen = false;
+        }
+        public void Einde()
+        {
+            AantalBeurten++;
+            if (AantalBeurten == 3)
+                einde = true;
+            else einde = false;
+        }
+    }
+}
---
+++ Spellen/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raadspelletje;
+
+namespace Spellen
+{
+    class Program
+    {
+        static Raadspel raadspel = new Raadspel();
+        static void Main(string[] args)
+        {
+            Boolean stopen = false;
+            while (!stopen)
+            {
+                Boolean einde = false;
+                Console.WriteLine("Druk ENTER om te beginen.");
+                Console.ReadLine();
+                raadspel.Worp();
+                raadspel.AantalBeurten = 1;
+                while (einde == false)
+                {
+                    int gok;
+                    try
+                    {
+                        Console.WriteLine("Beurt {0}: Geef een gok in tussen 1 en 10.", raadspel.AantalBeurten);
+                        gok = Convert.ToInt32(Console.ReadLine());
+                        raadspel.Gok(gok);
+                        if (raadspel.gewonnen == true)
+                            einde = true;
+                        else if (raadspel.einde == true)
+                            einde = true;
+                        raadspel.Einde();
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Incorrecte invoer");
+                    }
+
+                }
+                if (raadspel.gewonnen == true)
+                    Console.WriteLine("Proficiat je hebt het juiste getal geraden.");
+                else
+                    Console.WriteLine("Jammer genoeg heeft u het incorrecte getal geraden");
+                Console.WriteLine("Als je wil stoppen voer S in.");
+                String jaOfNee = Console.ReadLine().ToUpper();
+                if (jaOfNee == "S")
+                    stopen = true;
+            }
+        }
+
+    }
+}
